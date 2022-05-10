1be0da97dabdc20b3ca9d840e67794b5a4f93836
Nick-Kolchugin <drnik71@gmail.com>
2022-05-10 15:48:41 +0600

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //Комментарий для нового коммита
         System.out.println("Вывод текста");
         System.out.println("Вывод текста2");
         System.out.println("Вывод текста3");
