db1555e17ce6665bae907eb7c439d2305b449053
Nick-Kolchugin <drnik71@gmail.com>
2022-05-10 15:22:51 +0600

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Вывод текста");
+        System.out.println("Вывод текста2");
+        System.out.println("Вывод текста3");
     }
 }
